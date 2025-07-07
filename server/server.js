019b590e8cbb228fb4cc59f5d0610c56f2e02e9f
Mohitpandey003 <mohitraj2022003@gmail.com>
2025-07-07 20:56:11 +0530

    Prepare backend for Render deployment

--- server/server.js
+++ server/server.js
@@ -5,9 +5,25 @@ import connectDB from "./config/mongodb.js"
 import userRouter from "./routes/userRoutes.js"
 import imageRouter from "./routes/imageRoutes.js"
 dotenv.config()
-const port=process.env.port || 4000
+const port = process.env.PORT || 4000
 const app=express()
-await connectDB()
+const startServer = async () => {
+  await connectDB();
+
+  app.use(cors());
+  app.use(express.json());
+
+  app.use("/api/user", userRouter);
+  app.use("/api/image", imageRouter);
+  app.get('/', (req, res) => { res.send("API is working"); });
+
+  app.listen(port, () => {
+    console.log("App is running on port " + port);
+  });
+};
+
+startServer();
+
 
 app.use(cors())
 app.use(express.json())
